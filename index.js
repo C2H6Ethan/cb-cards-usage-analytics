d6a5bc519ce7ee547d444b051ed0d7b7b3d82b95
Ethan Baumgartner <ethan.baumgartner@roche.com>
2023-05-22 15:33:16 +0200

    fixed cors issue

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 const routes = require('./routes/routes');
+var cors = require('cors');
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
@@ -17,10 +18,12 @@ database.once('connected', () => {
 });
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api', routes);
 
-app.listen(3000, () => {
-	console.log(`Server Started at ${3000}`);
+app.listen(3001, () => {
+	console.log(`Server Started at ${3001}`);
 });
